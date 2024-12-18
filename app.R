6129c70b0918aeb3d263f6ccb842cea32a6c62b8
gabriel <141967985+gr4b0wski@users.noreply.github.com>
2024-12-18 22:19:59 +0100

    Add files via upload

--- app.R
+++ app.R
@@ -1,121 +1,154 @@
 library(shiny)
+library(ggplot2)
+library(plotly)
 
-pgn_file <- "lichess_wittchen_2024-12-18.pgn"
-pgn_text <- readLines(pgn_file)
-pgn_text <- pgn_text[nzchar(pgn_text)] 
-
+# Funkcje ekstrakcji danych
 extract_white_players <- function(pgn_lines) {
   white_lines <- grep("^\\[White \"[^\"]+\"\\]$", pgn_lines, value = TRUE)
-  white_players <- sub('^\\[White "(.*?)"\\]$', '\\1', white_lines)
-  return(white_players)
+  sub('^\\[White "(.*?)"\\]$', '\\1', white_lines)
 }
 extract_black_players <- function(pgn_lines) {
   black_lines <- grep("^\\[Black \"[^\"]+\"\\]$", pgn_lines, value = TRUE)
-  black_players <- sub('^\\[Black "(.*?)"\\]$', '\\1', black_lines)
-  return(black_players)
+  sub('^\\[Black "(.*?)"\\]$', '\\1', black_lines)
 }
 extract_results <- function(pgn_lines) {
   results <- grep("^\\[Result \"[^\"]+\"\\]$", pgn_lines, value = TRUE)
-  results <- sub('^\\[Result "(.*?)"\\]$', '\\1', results)
-  return(results)
+  sub('^\\[Result "(.*?)"\\]$', '\\1', results)
 }
 extract_dates <- function(pgn_lines) {
   dates <- grep("^\\[UTCDate \"[^\"]+\"\\]$", pgn_lines, value = TRUE)
-  dates <- sub('^\\[UTCDate "(.*?)"\\]$', '\\1', dates)
-  return(dates)
+  sub('^\\[UTCDate "(.*?)"\\]$', '\\1', dates)
 }
 extract_white_elo <- function(pgn_lines) {
   white_elo <- grep("^\\[WhiteElo \"[^\"]+\"\\]$", pgn_lines, value = TRUE)
-  white_elo <- sub('^\\[WhiteElo "(.*?)"\\]$', '\\1', white_elo)
-  return(white_elo)
+  sub('^\\[WhiteElo "(.*?)"\\]$', '\\1', white_elo)
 }
 extract_black_elo <- function(pgn_lines) {
   black_elo <- grep("^\\[BlackElo \"[^\"]+\"\\]$", pgn_lines, value = TRUE)
-  black_elo <- sub('^\\[BlackElo "(.*?)"\\]$', '\\1', black_elo)
-  return(black_elo)
+  sub('^\\[BlackElo "(.*?)"\\]$', '\\1', black_elo)
 }
 extract_eco <- function(pgn_lines) {
   eco <- grep("^\\[ECO \"[^\"]+\"\\]$", pgn_lines, value = TRUE)
-  eco <- sub('^\\[ECO "(.*?)"\\]$', '\\1', eco)
-  return(eco)
+  sub('^\\[ECO "(.*?)"\\]$', '\\1', eco)
 }
 extract_opening <- function(pgn_lines) {
   opening <- grep("^\\[Opening \"[^\"]+\"\\]$", pgn_lines, value = TRUE)
-  opening <- sub('^\\[Opening "(.*?)"\\]$', '\\1', opening)
-  return(opening)
+  sub('^\\[Opening "(.*?)"\\]$', '\\1', opening)
 }
 
-white <- extract_white_players(pgn_text)
-black <- extract_black_players(pgn_text)
-result <- extract_results(pgn_text)
-date <- extract_dates(pgn_text)
-white_elo <- extract_white_elo(pgn_text)
-black_elo <- extract_black_elo(pgn_text)
-eco <- extract_eco(pgn_text)
-opening_variation <- extract_opening(pgn_text)
-opening <- sub("\\:.*", "", opening_variation)
+# Wczytaj dane graczy
+load_data <- function(filepath, player_name) {
+  pgn_text <- readLines(filepath)
+  pgn_text <- pgn_text[nzchar(pgn_text)]
+  
+  white <- extract_white_players(pgn_text)
+  black <- extract_black_players(pgn_text)
+  result <- extract_results(pgn_text)
+  date <- extract_dates(pgn_text)
+  white_elo <- extract_white_elo(pgn_text)
+  black_elo <- extract_black_elo(pgn_text)
+  eco <- extract_eco(pgn_text)
+  opening_variation <- extract_opening(pgn_text)
+  opening <- sub("\\:.*", "", opening_variation)
+  
+  data <- data.frame(white, black, result, date, white_elo, 
+                     black_elo, opening, opening_variation, eco)
+  data <- data[data$white == player_name | data$black == player_name, ]
+  data$date <- as.Date(data$date, format = "%Y.%m.%d")
+  data$elo <- as.numeric(ifelse(data$white == player_name, 
+                                data$white_elo, data$black_elo))
+  data[order(data$date), ]
+}
 
-dane <- data.frame(white, black, result, date, white_elo, 
-                   black_elo, opening, opening_variation, eco)
+# Załaduj dane graczy
+wittchen_data <- load_data("lichess_wittchen_2024-12-18.pgn", "wittchen")
+tadziolul_data <- load_data("lichess_tadziolul_2024-12-18.pgn", "tadziolul")
 
-
-#KUBAAA:
-
-pgn_text2 <- readLines("lichess_tadziolul_2024-12-18.pgn")
-pgn_text2 <- pgn_text2[nzchar(pgn_text2)] 
-
-
-white2 <- extract_white_players(pgn_text2)
-black2 <- extract_black_players(pgn_text2)
-result2 <- extract_results(pgn_text2)
-date2 <- extract_dates(pgn_text2)
-white_elo2 <- extract_white_elo(pgn_text2)
-black_elo2 <- extract_black_elo(pgn_text2)
-eco2 <- extract_eco(pgn_text2)
-opening_variation2 <- extract_opening(pgn_text2)
-opening2 <- sub("\\:.*", "", opening_variation2)
-
-dane2 <- data.frame(white2, black2, result2, date2, white_elo2, 
-                    black_elo2, opening2, opening_variation2, eco2)
-
-
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+# UI
+ui <- navbarPage(
+  "Chess Stats",
+  tabPanel(
+    "Wittchen",
+    fluidPage(
+      titlePanel("Statystyki gracza Wittchen"),
+      fluidRow(
+        column(5, dateRangeInput("dateRangeW", "Wybierz zakres dat:", 
+                                 start = min(wittchen_data$date), 
+                                 end = max(wittchen_data$date),
+                                 min = min(wittchen_data$date), 
+                                 max = max(wittchen_data$date))),
+        column(3, selectInput("openingW", "Wybierz otwarcie:", 
+                              choices = c("Wszystkie" = "all", unique(wittchen_data$opening)))),
+        column(4, htmlOutput("statsW"))
+      ),
+      plotlyOutput("ratingPlotW", height = "500px")
     )
+  ),
+  tabPanel(
+    "Tadziolul",
+    fluidPage(
+      titlePanel("Statystyki gracza Tadziolul"),
+      fluidRow(
+        column(5, dateRangeInput("dateRangeT", "Wybierz zakres dat:", 
+                                 start = min(tadziolul_data$date), 
+                                 end = max(tadziolul_data$date),
+                                 min = min(tadziolul_data$date), 
+                                 max = max(tadziolul_data$date))),
+        column(3, selectInput("openingT", "Wybierz otwarcie:", 
+                              choices = c("Wszystkie" = "all", unique(tadziolul_data$opening)))),
+        column(4, htmlOutput("statsT"))
+      ),
+      plotlyOutput("ratingPlotT", height = "500px")
+    )
+  )
 )
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+# Server
+server <- function(input, output, session) {
+  
+  render_player_data <- function(data, dateRangeInput, openingInput, statsOutput, plotOutput) {
+    filteredData <- reactive({
+      data_filtered <- data[data$date >= input[[dateRangeInput]][1] &
+                              data$date <= input[[dateRangeInput]][2], ]
+      if (input[[openingInput]] != "all") {
+        data_filtered <- data_filtered[data_filtered$opening == input[[openingInput]], ]
+      }
+      data_filtered
     })
+    
+    output[[statsOutput]] <- renderUI({
+      data_filtered <- filteredData()
+      won <- sum((data_filtered$result == "1-0" & data_filtered$white == data_filtered$white[1]) |
+                   (data_filtered$result == "0-1" & data_filtered$black == data_filtered$black[1]))
+      draw <- sum(data_filtered$result == "1/2-1/2")
+      lost <- sum((data_filtered$result == "0-1" & data_filtered$white == data_filtered$white[1]) |
+                    (data_filtered$result == "1-0" & data_filtered$black == data_filtered$black[1]))
+      HTML(paste0("<span style='color:green;'>", won, "</span> - ",
+                  "<span style='color:orange;'>", draw, "</span> - ",
+                  "<span style='color:red;'>", lost, "</span>"))
+    })
+    
+    output[[plotOutput]] <- renderPlotly({
+      data_filtered <- filteredData()
+      plot <- ggplot(data_filtered, aes(x = date, y = elo)) +
+        geom_line(color = "black", linewidth = 0.5) +
+        geom_point(aes(
+          text = paste0(
+            "Data: ", date, "<br>",
+            "Ranking: ", elo, "<br>",
+            "Result: ", result
+          )
+        ), color = "black", size = 1.5) +
+        labs(x = "Data", y = "Ranking", title = paste("Ranking gracza", data_filtered$white[1])) +
+        theme_minimal()
+      ggplotly(plot, tooltip = "text") %>%
+        layout(hoverlabel = list(align = "left"))
+    })
+  }
+  
+  render_player_data(wittchen_data, "dateRangeW", "openingW", "statsW", "ratingPlotW")
+  render_player_data(tadziolul_data, "dateRangeT", "openingT", "statsT", "ratingPlotT")
 }
 
-# Run the application 
+# Run app
 shinyApp(ui = ui, server = server)
